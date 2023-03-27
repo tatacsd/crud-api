a945f033f04fe5644689202c7cea31cb88bfb908
thayscasado <thayscasado@hotmail.com>
2023-03-26 22:15:01 -0700

    🌱 feat: Add seed function to populate database
    This commit adds a new function to the application that populates the database with an initial set of tasks.

--- src/database.js
+++ src/database.js
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto';
 import fs from 'node:fs/promises';
 const databasePath = new URL('../db.json', import.meta.url);
 export class Database {
@@ -12,6 +13,7 @@ export class Database {
       .catch((err) => {
         console.log(err);
         // if the file doesn't exist, create it
+        this.#seed();
         this.#persist();
       });
   }
@@ -68,5 +70,59 @@ export class Database {
       };
       this.#persist();
     }
+  }
+
+  #seed() {
+    this.#database = {
+      tasks: [
+        {
+          id: randomUUID(),
+          title: 'Build a RESTful API with Node.js',
+          description:
+            'Create a RESTful API using Node.js and a database of your choice',
+          completed_at: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: randomUUID(),
+          title: 'Implement user authentication with Node.js',
+          description:
+            'Add user authentication to a Node.js application using Passport.js or a similar library',
+          completed_at: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: randomUUID(),
+          title: 'Build a real-time chat application with Node.js',
+          description:
+            'Create a chat application that allows users to communicate in real-time using Node.js and Socket.io',
+          completed_at: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: randomUUID(),
+          title: 'Create a web scraper with Node.js',
+          description:
+            'Build a web scraper that can extract data from a website using Node.js and a scraping library like Cheerio',
+          completed_at: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          id: randomUUID(),
+          title: 'Build a command-line tool with Node.js',
+          description:
+            'Create a command-line tool that can perform a useful task using Node.js and a CLI library like Commander.js',
+          completed_at: null,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+    };
+
+    this.#persist();
   }
 }
