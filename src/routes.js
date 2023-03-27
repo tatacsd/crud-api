ced99bee33dcf38d2eab2aabbe5f677a11202aa9
thayscasado <thayscasado@hotmail.com>
2023-03-26 21:38:00 -0700

    feat: Add task creation endpoint with database insert
    This commit adds a new endpoint for creating tasks in the application. The endpoint accepts a POST request to the /tasks URL and expects a JSON payload containing a title and description field.

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,8 @@
+import { randomUUID } from 'node:crypto';
+import { Database } from './database.js';
+
+const database = new Database();
+
 export const routes = [
   {
     method: 'GET',
@@ -14,11 +19,28 @@ export const routes = [
     method: 'POST',
     url: '/tasks',
     handler: (req, res) => {
-      return res.end(
-        JSON.stringify({
-          message: 'POST /tasks',
-        })
-      );
+      // TODO: check if the body is valid
+      if (!req.body.title || !req.body.description) {
+        return res.writeHead(400).end(
+          JSON.stringify({
+            message: 'Title and description are required',
+          })
+        );
+      }
+
+      // TODO: insert the task into the database
+      const task = database.insert('tasks', {
+        id: randomUUID(),
+        title: req.body.title,
+        description: req.body.description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      });
+
+      // TODO: return the task
+      res.writeHead(201);
+      return res.end(JSON.stringify(task));
     },
   },
   {
